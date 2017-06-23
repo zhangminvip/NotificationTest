2c79c8cbbc5b4391d7a7dbfbe8f90a61ce33db3b
zhangminvip <2107609379@qq.com>
2017-06-23 21:23:08 +0800

    Add function

--- app/src/main/java/com/example/notificationtest/Notification.java
+++ app/src/main/java/com/example/notificationtest/Notification.java
@@ -2,7 +2,9 @@ package com.example.notificationtest;
 
 import android.app.Activity;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.support.v4.app.NotificationCompat;
 
@@ -15,15 +17,26 @@ import static android.content.Context.NOTIFICATION_SERVICE;
 public class Notification {
 
     public static void sendNotification(){
+        Intent intent = new Intent(MyApplication.getContext(),NotificationActivity.class);
+        PendingIntent pi = PendingIntent.getActivity(MyApplication.getContext(),0,intent,0);
         NotificationManager manager = (NotificationManager)MyApplication.getContext()
                 .getSystemService(NOTIFICATION_SERVICE);
-        android.app.Notification notification = new NotificationCompat.Builder(MyApplication.getContext())
+        android.app.Notification notification = new NotificationCompat.Builder(MyApplication.getContext() )
                 .setContentTitle("this is content title")
                 .setContentText("this is content text")
                 .setWhen(System.currentTimeMillis())
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .setLargeIcon(BitmapFactory.decodeResource(MyApplication.getContext().getResources(),
                         R.mipmap.ic_launcher))
+                .setContentIntent(pi)
+                .setAutoCancel(true)
+                .setStyle(new NotificationCompat.BigPictureStyle()
+                        .bigPicture(BitmapFactory
+                                .decodeResource(MyApplication.getContext()
+                                        .getResources(),R.drawable.girl)))
+
+                .setDefaults(NotificationCompat.DEFAULT_ALL)
+                .setPriority(NotificationCompat.PRIORITY_MAX)
                 .build();
 
 
---
+++ app/src/main/java/com/example/notificationtest/NotificationActivity.java
@@ -0,0 +1,18 @@
+package com.example.notificationtest;
+
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by tom on 2017/6/23.
+ */
+
+public class NotificationActivity  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notification_layout);
+    }
+}
